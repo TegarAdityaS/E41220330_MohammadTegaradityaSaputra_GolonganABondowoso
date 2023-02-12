e4ff53977b5dda517b35b4878d74a9f4665167c0
TegarAdityaS <mohammadtegaradityasaputra@gmail.com>
2023-02-12 20:52:46 +0700

    Create TugasMinggu1.java

---
+++ minggu1/tugasMinggu1/src/tugasminggu1/TugasMinggu1.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasminggu1;
+
+/**
+ *
+ * @author TEGAR ADITYA
+ */
+public class TugasMinggu1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama =("Mohammad Tegar Aditya Saputra");
+        String NIM =("E41220330");
+        int semester =1;
+        String Prodi =("Teknik Informatika");
+        String Jurusan =("Teknologi informasi");
+        String ttl =("Bondowoso, 29 Maret 2004");
+        String Alamat =("Desa Pancoran, Kecamatan Bondowoso, Kabupaten Bondowoso");
+        String Hobi =("Olahraga Bulu Tangkis");
+        
+        System.out.println("BIODATA SAYA : ");
+        System.out.println("---------------");
+        System.out.println("Nama \t\t\t\t :"+nama);
+        System.out.println("NIM \t\t\t\t :"+NIM);
+        System.out.println("Semester \t\t\t :"+semester);
+        System.out.println("PRODI \t\t\t\t :"+Prodi);
+        System.out.println("JURUSAN \t\t\t :"+Jurusan);
+        System.out.println("Tempat Tanggal Lahir \t\t :"+ttl);
+        System.out.println("Alamat \t\t\t\t :"+Alamat);
+        System.out.println("HOBI \t\t\t\t :"+Hobi);
+    }
+    
+}
